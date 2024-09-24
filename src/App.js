c36c1ac0e9de79354ec1ca43be12de29764c11e3
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2024-09-24 23:02:37 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import React, { Component ,Fragment} from 'react'
 import Navbar from './components/Navbar';
 import Newscss from './components/Newscss';
 export default class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Newscss/>
-      </Fragment>
+          <Router>
+          <Navbar title="Quizer" aboutText="About Us" mode="black"/>
+          <Routes>
+          <Route path="/news" exact element={<Newscss/>}/>
+           </Routes>
+          </Router>
     )
   }
 }
